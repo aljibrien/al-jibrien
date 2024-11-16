89d3a2024199a8949c9c18d42dcd6e4da7681e7b
aljibrien <118417878+aljibrien@users.noreply.github.com>
2024-11-16 16:44:26 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const navbarNav = document.querySelector('.navbar');
 document.querySelector('#menu').onclick = () =>{
     navbarNav.classList.toggle('active');
 }
+
+const hamburger = document.querySelector('#menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target)&& !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active')
+    }
+});
